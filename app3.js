5317e3cba5b155270f22fd26d3248c1cb1fa04ad
setsky95 <sebapapavlotsky@gmail.com>
2023-05-13 18:31:57 -0300

    Botones de eliminar, carrito visible y estilos

--- app3.js
+++ app3.js
@@ -7,15 +7,24 @@ window.addEventListener('load', function () {
     { id: 1, titulo: "Raices" , icono:"🍔" ,precio:2500, ingredientes:"Pan de papa, queso cheddar, carne 100% vacuna, pepino", disponible: true, img:"./imgs/RaicesChica.webp"} ,
     { id: 2, titulo: "Autorretrato", icono:"🍔"  , precio:1800, ingredientes:"Pan de papa, queso cheddar, carne 100% vacuna, bacon", disponible: true, img:"./imgs/autotriplechica.webp" } ,
     { id: 3, titulo: "Vincent", icono:"🍔"  , precio:2400, ingredientes: "Pan de papa, cuatro quesos, carne 100% vacuna, salsa criolla", disponible: true, img:"./imgs/Vangoghchica.webp" } ,
-    {   id: 4, titulo: "Girasoles", icono:"🍔"  , precio:2000, ingredientes: "Pan de papa, medallón NotBurger, cebolla grillada, champis", disponible: true , img:"./imgs/girasoleschica.webp"} ,
-    { id: 5, titulo: "Vangogh", icono:"🍟"  , precio:900, ingredientes: "Papas fritas, queso cheddar, panceta, crema y verdeo ", disponible: true , img:"./imgs/papas_vangogh.webp"} ,
-    { id: 6, titulo: "Cheddar", icono:"🍟"  , precio:1100, ingredientes: "Papas fritas, queso cheddar, panceta, crema y verdeo.    ", disponible: true , img:"./imgs/papas_cheddar.webp"} ,
-  
+    { id: 4, titulo: "Girasoles", icono:"🍔"  , precio:2000, ingredientes: "Pan de papa, medallón NotBurger, cebolla grillada, champis", disponible: true , img:"./imgs/girasoleschica.webp"} ,
+    { id: 5, titulo: "Noche Estrellada", icono:"🍔"  , precio:1900, ingredientes: "Pan de papa, medallón de carne, cebolla caramelizada, cheddar", disponible: true , img:"./imgs/viñedochica.webp"} ,
+    { id: 6, titulo: "Cuervos", icono:"🍔"  , precio:2150, ingredientes: "Pan de papa, medallón de carne, que azul, queso crema, nuez", disponible: true , img:"./imgs/CuervosChica.webp"} ,
+    { id: 7, titulo: "Pollo grill", icono:"🥪"  , precio:1400, ingredientes: "Pan de viena, pollo en cubos,cebolla  y cheddar", disponible: true , img:"./imgs/sangu_de_pollo.webp"} ,
+    { id: 8, titulo: "Philly steak", icono:"🥪"  , precio:1400, ingredientes: "Pan de viena, carne asada en tiras, cebolla  y cheddar", disponible: true , img:"./imgs/Philly_steak.webp"} ,
+    { id: 9, titulo: "Nuggets", icono:"🥨"  , precio:1100, ingredientes: "Nuggets de pollo fritos con diversas salsas", disponible: true , img:"./imgs/Nuggets-pollo.webp"} ,
+    { id: 10, titulo: "Aros de cebolla", icono:"🥨"  , precio:1000, ingredientes: "Aros empanados fritos, con barbacoa casera", disponible: true , img:"./imgs/onion.rings.webp"} ,
+    { id: 11, titulo: "Vangogh", icono:"🍟"  , precio:1100, ingredientes: "Papas fritas, queso cheddar, panceta, crema y verdeo. Especiales de la casa  ", disponible: true , img:"./imgs/papas_vangogh.webp"} ,
+    { id: 12, titulo: "Cheddar", icono:"🍟"  , precio:1100, ingredientes: "Papas fritas, queso cheddar, panceta, crema y verdeo.    ", disponible: true , img:"./imgs/papas_cheddar.webp"} ,
+
   ]
-  
-  /* MENÚ DINÁMICO EN */
-  
-  const contenedorProductos = document.querySelector ("#contenedorProductos")
+
+
+  //////////////////////////////////////////////
+//FOR EACH PARA RENDERIZAR TODO EL ARRAY HAMBURGUESAS
+//////////////////////////////////////////////
+
+  const contenedorProductos = document.querySelector (".contenedorProductos")
   
 
   hamburguesas.forEach(hamburguesa =>  { 
@@ -23,14 +32,14 @@ window.addEventListener('load', function () {
     contenedor.classList.add(`hamburguesa`);
     contenedor.innerHTML= 
     `
-    <div class="card  border-dark card-sm ">
-    <img src="${hamburguesa.img}" class="card-img-top" alt="viñedo">
-  <div class="my-2 text-center card   ">
-    <div class="card-body">
-      <h2 class="text-center  ">${hamburguesa.titulo}</h2>
-      <p class="text-center  ">${hamburguesa.ingredientes}</p>
-      <p class="text-center  ">$${hamburguesa.precio}</p>
-      <button  class="pusheoAlcarrito btn btn-dark p-1">Sumar al pedido</a> 
+    <div class=" rounded-5 ">
+    <img src="${hamburguesa.img}" class="card-img-top rounded-1" alt="viñedo">
+  <div class="my-2 text-center card"  >
+    <div class="card-body fondo_terraza "  style="height: 12rem;" >
+      <h2 class="text-center texto-web my-0">${hamburguesa.titulo}</h2>
+      <p class="text-center texto-carrito my-0 interlineado-menu"  style="height: 3rem;">${hamburguesa.ingredientes}</p>
+      <p class="text-center texto-carrito my ">$${hamburguesa.precio}</p>
+      <button  class="pusheoAlcarrito btn btn-dark btn-outline-light px-1 py-0">Sumar al pedido</a> 
     </div>
   </div>
   </div>
@@ -40,145 +49,160 @@ window.addEventListener('load', function () {
   
   const carrito = []
   
-  
-  /* PUSH X INDICE 
-  carrito.push(hamburguesas[0]); 
-  carrito.push(hamburguesas[1]); 
-  carrito.push(hamburguesas[2]); 
-  carrito.push(hamburguesas[3]); 
-  carrito.push(hamburguesas[4]);  
+    //////////////////////////////////////////////
+// CONDICIONAL PARA MOSTRAR CARRITO O NO //
+//////////////////////////////////////////////
+
+const mostrarCarrito = document.getElementById ("carritoOn")
+const mostrarCierre = document.getElementById("finishOn")
+const backHomeBtn = document.getElementById ("backHomeBtn")
+function ocultarCarritovacio  () {
+if (carrito.length >= 1) {
+mostrarCierre.style.display = "block";
+mostrarCarrito.style.display = "block";
+backHomeBtn.style.display = "none";
+
+ actualizarCarrito2 ();
+} else {
+backHomeBtn.style.display = "block";
+ mostrarCarrito.style.display = "none";
+ mostrarCierre.style.display = "none";
+ actualizarCarrito2 ();
+
+}
+}
+ocultarCarritovacio  ()
+  /* PUSH X INDICE  
   carrito.push(hamburguesas[5]);  
   */
+//////////////////////////////////////////////
+//FUNCION DE ACTUALIZACIÓN QUE ELIMINA LO RENDERIZADO SI TIENE UN HIJO 
+  //Y RENDERIZA DE NUEVO CON UN FOREACH
+//////////////////////////////////////////////
+
+
+
+  function actualizarCarrito2 (){
+
+    const contenedorCarrito = document.querySelector ("#contenedorCarrito")
+    while (contenedorCarrito.firstChild) {
+      contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
   
-  /*Funcioón de actulalizacion*/
+  carrito.forEach(productoElegido =>  { 
+    const contenedor2 = document.createElement(`article`);
+    contenedor2.classList.add(`productoElegido`);
+    contenedor2.innerHTML = `
+    
+    <div class=" row  row-cols-md-3  g-3   border-dark card-sm bg-transparent  d-flex  justify-content-start align-items-center" ">
+    <div class=" d-flex justify-content-start align-items-center">
+    <h2 class="texto-carrito"> ${productoElegido.icono}  ${productoElegido.titulo}  </h2></div>
+
+    <div class=" d-flex justify-content-between align-items-center">
+    
+    <button type="button" class="borrarDelcarrito btn btn-dark p-1">Borrar del pedido</a> 
+    </div>
+    <div class=" d-flex justify-content-end align-items-center">
+    <h3 class="texto-carrito "> $${productoElegido.precio}  </h3>
+</div>
+</div>
 
 
-   function actualizarCarrito (){
-    let carritoFinal =carrito.map(burger =>{
-      return `${burger.icono}  ${burger.titulo}`  ;
-    });  const precioFinal = carrito.reduce((acumulado, burger) => {
-      return acumulado + burger.precio;
-    }, 0);
-    const precioCarrito = document.querySelector("#precioCarrito").textContent = ` total: ${precioFinal} `;
-    const textoCarrito = document.querySelector("#carritofinal").textContent = `Tu pedido es: ${carritoFinal} `;
-   }
+    `;
 
-
-  
-
-
-document.querySelectorAll(`.pusheoAlcarrito`).forEach((boton, index) => {
-  boton.addEventListener(`click`,()=>{
-    carrito.push(hamburguesas[index])
-    actualizarCarrito ()
-
-  })
+    contenedorCarrito.appendChild (contenedor2)
+  });
 }
-)
-
-  /* BOTONES PARA PUSHEAR AL ARRAY CARRITO*/
-
-
-/* 
-carrito.forEach((burgers, index) => {
-  console.log (index)
-}) */
 
 
 
- /*  
-
-  const botonVincent = document.querySelector("#btn3");
-  botonVincent.onclick = function () {
-    pushAlcarrito()
-  }
-  const botonGirasoles = document.querySelector("#btn4");
-  botonGirasoles.onclick = function () {
-    carrito.push(hamburguesas[3]); 
-      let carritoFinal =carrito.map(burger =>{
-      return `${burger.icono}  ${burger.titulo}`  ;
-    });  const precioFinal = carrito.reduce((acumulado, burger) => {
-      return acumulado + burger.precio;
-    }, 0);
-    const precioCarrito = document.querySelector("#precioCarrito").textContent = ` total: ${precioFinal} `;
-    const textoCarrito = document.querySelector("#carritofinal").textContent = `Tu pedido es: ${carritoFinal} `;
-  }
-  
-  const botonpapas1= document.querySelector("#btn5");
-  botonpapas1.onclick = function () {
-    carrito.push(hamburguesas[4]); 
-    let carritoFinal =carrito.map(burger =>{
-      return `${burger.icono}  ${burger.titulo}`  ;
-    });  const precioFinal = carrito.reduce((acumulado, burger) => {
-      return acumulado + burger.precio;
-    }, 0);
-    const precioCarrito = document.querySelector("#precioCarrito").textContent = ` total: ${precioFinal} `;
-    const textoCarrito = document.querySelector("#carritofinal").textContent = `Tu pedido es: ${carritoFinal} `;
-  }
-  const botonPapas2 = document.querySelector("#btn6");
-  botonPapas2.onclick = function () {
-    carrito.push(hamburguesas[5]); 
-    let carritoFinal =carrito.map(burger =>{
-      return `${burger.icono}  ${burger.titulo}`  ;
-    });  const precioFinal = carrito.reduce((acumulado, burger) => {
-      return acumulado + burger.precio;
-    }, 0);
-    const precioCarrito = document.querySelector("#precioCarrito").textContent = ` total: ${precioFinal} `;
-    const textoCarrito = document.querySelector("#carritofinal").textContent = `Tu pedido es: ${carritoFinal} `;
-  }
-  
-   
-   */
-  
-  /* VACIAR EL CARRITO Guardar los datos en una variable (para algo servirá)  */
-  
-  
-  const vaciarCarrito = document.querySelector("#btnVaciado");
-  vaciarCarrito.onclick = function () {
-  const carritoVaciado = carrito.splice(0)
-  const precioCarrito = document.querySelector("#precioCarrito").textContent = `  `;
-  const textoCarrito = document.querySelector("#carritofinal").textContent = `El plato está vacío`;
-  }
-  
-  
-  /* ElIMINAR UN ELEMENTO A PARTIR DEL INDICE
-  carrito.splice(0,1);
-  carrito.splice(1,1);
-  carrito.splice(2,1);
-  carrito.splice(3,1);
-  carrito.splice(4,1);
-  carrito.splice(5,1);
-  */
-  
-  
-  
-  /* SUMA DEL CARRITO */
+//////////////////////////////////////////////
+//  SUMA DEL CARRITO //
+//////////////////////////////////////////////
+function actualizarPreciofinal () {
   let precioFinal = carrito.reduce((acumulado, burger)=>{
-      return acumulado + burger.precio
+    return acumulado + burger.precio
   }, 0)
   
-  
-  /* COMPOSICIÓN DEL CARRITO */
-  
-  let carritoFinal =carrito.map(burger =>{
-    return `${burger.icono}  ${burger.titulo}`  ;
-  
+    const precioCarrito = document.getElementById("precioCarrito")
+    precioCarrito.innerHTML= `
+    <h2 class="texto-carrito text-center"> TOTAL $ ${precioFinal}  </h2>
+   
+    ` 
+  }
+
+
+///////////////////////////////////////////////
+  //CICLO FOREACH PARA PUSHEAR AL CARRITO 
+//////////////////////////////////////////////
+document.querySelectorAll(`.pusheoAlcarrito`).forEach((boton, index) => {
+  boton.addEventListener(`click`, () => {
+    carrito.push(hamburguesas[index]);
+    ocultarCarritovacio  ();
+    actualizarCarrito2();
+    actualizarPreciofinal ();
+
+
   });
+});
+
+
+ /////////////////////////////////////////////
+ // VACIAR EL CARRITO Guardar los datos en una variable (para algo servirá)  //
+  //////////////////////////////////////////////
+
   
+  const vaciarCarrito = document.getElementById('btnVaciado');
+  vaciarCarrito.onclick = function () {
+  const carritoVaciado = carrito.splice(0)
+  actualizarCarrito2 ();
+  actualizarPreciofinal ();
+  ocultarCarritovacio  ();
+
+  const textoCarrito = document.querySelector("#precioCarrito").textContent = `El plato está vacío`;
+  }
+    //////////////////////////////////////////////
+// ElIMINAR UN ELEMENTO A PARTIR DE UN EVENTO EN EL PADRE DEL HTML
+//( NO FUNCIONABA DIRECTAMENTE SOBRE EL BOTON YA QUE NO ESTABA CARGADO)//
+    //////////////////////////////////////////////
+
+    const contenedorCarrito = document.querySelector("#contenedorCarrito");
+
+    contenedorCarrito.addEventListener("click", (event) => {
+      if (event.target.classList.contains("borrarDelcarrito")) {
+        const boton = event.target;
+        const producto = boton.closest(".productoElegido");
+    
+        const index = Array.from(contenedorCarrito.children).indexOf(producto);
+    
+    
+          carrito.splice(index, 1);
+          actualizarCarrito2();
+          actualizarPreciofinal();
+          ocultarCarritovacio  ();
+      }
+    });
+
+
+
+
+ // MUESTRA DE PRODUCTOS EN CARRITO  //
   
-  /* MUESTRA DE PRODUCTOS EN CARRITO  */
-  
-  const textoCarrito = document.querySelector("#carritofinal").textContent=
+/*   const textoCarrito = document.querySelector("#carritofinal").textContent=
   `El plato está vacío`
-  
-  const precioCarrito = document.querySelector("#precioCarrito").textContent=
-  ``
+   */
   
   
+    //////////////////////////////////////////////
+
+
+
+  //CONSOLA //
+/*   
   console.log(carrito)
-  console.log(`el total es ${precioFinal}`)
   console.log(`el pedido es ${carritoFinal}`)
-  
+   */
   
   
   })
